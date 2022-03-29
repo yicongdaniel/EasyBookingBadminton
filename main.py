a1ef3380e86dc8da219db62d3bc6a0a4fa488c67
Daniel Zhang <danzhang@ciena.com>
2022-03-29 11:39:40 -0400

    Added for loop for selecting more than 1 courts

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -7,75 +6,69 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-# Please enter your Email and password in the variables: For example Email = "danielyicong@gmail.com"  Password = "123456789" 
+# Please enter your Email and password in the variables: 
+# For example 
+# Email = "danielyicong@gmail.com"  
+# Password = "123456789" 
 Email = "danielzyc9631@gmail.com"
 Password = "Minouandchoue2!"        
 PATH = "C:\chromedriver.exe"
+URL = "https://portailcitoyen.gatineau.ca/identite/Account/Login?ReturnUrl=%2Fidentite%2Fconnect%2Fauthorize%2Fcallback%3Fclient_id%3Dic3.web%26redirect_uri%3Dhttps%253A%252F%252Fcultureloisirs.gatineau.ca%252Fic3.production%252Fopenidauthcallback%26response_type%3Did_token%2520token%26scope%3Dopenid%2520profile%2520email%2520lazer%26state%3D7836e490ce534e37bd1436451800cba7%26nonce%3D2165cc51d19e444f91747bd139c65e94"
 driver = webdriver.Chrome(PATH)
 driver.maximize_window()
 
-# Go to web page
-driver.get("https://portailcitoyen.gatineau.ca/identite/Account/Login?ReturnUrl=%2Fidentite%2Fconnect%2Fauthorize%2Fcallback%3Fclient_id%3Dic3.web%26redirect_uri%3Dhttps%253A%252F%252Fcultureloisirs.gatineau.ca%252Fic3.production%252Fopenidauthcallback%26response_type%3Did_token%2520token%26scope%3Dopenid%2520profile%2520email%2520lazer%26state%3D7836e490ce534e37bd1436451800cba7%26nonce%3D2165cc51d19e444f91747bd139c65e94")
+# Log in part
+driver.get(URL)
 driver.find_element_by_name("Email").send_keys(Email)
 driver.find_element_by_name("Password").send_keys(Password)
 driver.find_element_by_css_selector("input[type=submit]").click()
 print("logged in successfully!\n")
 time.sleep(3)
+
+# Booking courts part
+# Phase the court's URL below. For example:
+# If you want to book only one court, do it like this:
+# courts_list = ["https://cultureloisirs.gatineau.ca/ic3.production/#/U5200/view/73376"]
+
+# If you want two courts do it like this
+# courts_list = ["https://cultureloisirs.gatineau.ca/ic3.production/#/U5200/view/73376", 
+#             "https://cultureloisirs.gatineau.ca/ic3.production/#/U5200/view/73292"]
+
+courts_list = ["https://cultureloisirs.gatineau.ca/ic3.production/#/U5200/view/73376", 
+            "https://cultureloisirs.gatineau.ca/ic3.production/#/U5200/view/73292"]
+
+court_num = 1
+for court in courts_list:
+    
+    driver.get(court)
+    driver.implicitly_wait(10) # seconds
+    myDynamicElement = driver.find_element_by_xpath("//*[@id='u5200_btnRegisterSecond']")
+    myDynamicElement.click()
+    print("Clicked Inscrire button\n")
+    
+    myDynamicElement = driver.find_element_by_xpath("//*[@id='u3600_btnSelect0']")
+    myDynamicElement.click()
+    print("Clicked person\n")
+    print ("Court ", court_num, " selected")
+    court_num += 1
+
+# Checkout part
 driver.implicitly_wait(10) # seconds
-# Go to court 1
-# Phase the 1st court's URL below
-# url_list = ["https://cultureloisirs.gatineau.ca/IC3.Production/#/U5200/view/73295", 
-#             "https://cultureloisirs.gatineau.ca/ic3.production/#/U5200/view/73376"]
-
-driver.get("https://cultureloisirs.gatineau.ca/IC3.Production/#/U5200/view/73295")
-print("To the court 1 page\n")
-myDynamicElement = driver.find_element_by_xpath("//*[@id='u5200_btnRegisterSecond']")
-myDynamicElement.click()
-print("Clicked Inscrire button\n")
-
-
-myDynamicElement = driver.find_element_by_xpath("//*[@id='u3600_btnSelect0']")
-myDynamicElement.click()
-print("Clicked person\n")
-
-# Go to court 2
-# Phase the 2ed court's URL below
-driver.get("https://cultureloisirs.gatineau.ca/ic3.production/#/U5200/view/73376")
-print("To the court 1 page\n")
-myDynamicElement = driver.find_element_by_xpath("//*[@id='u5200_btnRegisterSecond']")
-myDynamicElement.click()
-print("Clicked Inscrire button\n")
-
-
-myDynamicElement = driver.find_element_by_xpath("//*[@id='u3600_btnSelect0']")
-myDynamicElement.click()
-print("Clicked person\n")
-
-
 myDynamicElement = driver.find_element_by_xpath("//*[@id='u3600_btnCheckout0']")
 myDynamicElement.click()
 print("Clicked checkout button\n")
 
-
-# myDynamicElement = driver.find_element_by_xpath("//*[@id='u3600_btnCartShoppingCompleteStep']")
-# myDynamicElement.click()
-# print("Clicked Basket section completed button\n")
 time.sleep(2)
 button = driver.find_element_by_xpath("//*[@id='u3600_btnCartShoppingCompleteStep']")
 driver.execute_script("arguments[0].click();", button) 
 print("Clicked Basket section completed button\n")
 
-
 myDynamicElement = driver.find_element_by_xpath("//*[@id='u3600_chkElectronicPaymentCondition']")
 myDynamicElement.click()
 print("Checked terms and conditions \n")
 
-# driver.implicitly_wait(10) # seconds
-# myDynamicElement = driver.find_element_by_xpath("//*[@id='u3600_btnCartPaymentCancel']")
-# myDynamicElement.click()
-# print("Clicked Cancel button \n")
-
-# # driver.find_element_by_xpath("//*[@id='u3600_btnCartPaymentCompleteStep']").click()
-# # print("Clicked Confirm button\n")
+myDynamicElement = driver.find_element_by_xpath("//*[@id='u3600_btnCartPaymentCompleteStep']")
+myDynamicElement.click()
+print("Clicked Confirm button \n")
 
 
